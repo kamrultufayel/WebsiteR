cee48288f2914ed1758b320892bc7b4c175ba718
kamrultufayel <islam47249@gmail.com>
2023-10-24 01:25:21 +0100

    read

---
+++ M3.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index2.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        # Placeholder for form submission handling
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirm-password']
+        
+        # Placeholder: Add your signup logic here
+        
+        # For now, redirecting to the home page
+        return redirect(url_for('home'))
+
+    return render_template('signup.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
